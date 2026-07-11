61311dea69fd2f66481dc519ef5fafdce54acaf4
Sho Uno Sabesaje <155436471+ShoIchiiii@users.noreply.github.com>
2026-07-11 08:41:51 +0800

    Update DeleteReview.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/DeleteReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/DeleteReview.cs
@@ -1,21 +1,18 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-// EXERCISE 8: DELETE Remove Review
-// JSONPlaceholder API: https://jsonplaceholder.typicode.com/posts/{id}
-//
-// Your task:
-// 1. Send a DELETE request to remove post with ID 1
-// 2. Assert status code is 200 OK
-//
-// Hint: Use await client.DeleteAsync(url)
+namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class DeleteReview
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Send DELETE request to https://jsonplaceholder.typicode.com/posts/1
-        // TODO: Assert status code is 200 OK
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
+        var response = await client.DeleteAsync(url);
 
-        throw new NotImplementedException();
+        if (response.StatusCode != HttpStatusCode.OK)
+            throw new Exception($"Expected 200 OK, got {response.StatusCode}");
     }
 }
